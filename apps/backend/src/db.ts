a9fcfec99313314ed5a28d735afe4663d22a9fe2
gracm0 <grace.y.mahoney@gmail.com>
2025-10-03 13:47:24 -0400

    database connects

--- apps/backend/src/db.ts
+++ apps/backend/src/db.ts
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
+import "dotenv/config";
 
 async function connectDB() {
-    const { DB_USER, DB_PASS, MONGO_HOST, MONGO_PORT, MONGO_DB } = process.env;
+  console.log("Connecting to MongoDB...");
+    const { DB_USER, DB_PASS, MONGO_HOST, MONGO_DB } = process.env;
 
-    if (!DB_USER || !DB_PASS || !MONGO_HOST || !MONGO_PORT || !MONGO_DB) {
+    if (!DB_USER || !DB_PASS || !MONGO_HOST || !MONGO_DB) {
         throw new Error("Missing required environment variables");
     }
 
@@ -11,11 +13,22 @@ async function connectDB() {
     
   try {
     await mongoose.connect(uri, { dbName: MONGO_DB });
-    console.log("Connected to users collection");
+    console.log(`Connected to MongoDB database: ${MONGO_DB}`);
   } catch (err) {
-    console.error("Failed to connect to users collection", err);
+    console.error("Failed to connect to MongoDB", err);
     process.exit(1);
   }
 }
 
 export default connectDB;
+
+// TEMP TEST
+connectDB()
+  .then(() => {
+    console.log("DB connection test completed. Exiting...");
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
